79c278876649ca226a11479189e22ce611e48a2e
sskr365 <110958092+sskr365@users.noreply.github.com>
2023-06-17 15:01:00 +0530

    Add files via upload

--- JAVA/JavaProjects/Business/src/Person.java
+++ JAVA/JavaProjects/Business/src/Person.java
@@ -10,9 +10,16 @@ public class Person {
     static private Company company = new Company();
     private NewsAgency news;
 
+    public void sayHello(){
+        System.out.println("Heloo dudes");
+
+    }
+
 
     public static void main(String[] args) {
         //System.out.println(firstName); //no static key word
         System.out.println(company); //static key word
+        Person p1 = new Person();
+        p1.sayHello();
     }
 }
